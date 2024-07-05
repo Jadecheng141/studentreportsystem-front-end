bea023fb22360b0fd7785a3848eec62b96e3b933
chengruyun <isjade.cheng@gmail.com>
2024-07-05 11:16:53 +0800

    登录fix

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -101,21 +101,20 @@
 </template>
 
 <script>
-import { validUsername } from '@/utils/validate'
 
 export default {
   name: 'Login',
   data() {
     const validateUsername = (rule, value, callback) => {
-      if (!validUsername(value)) {
-        callback(new Error('Please enter the correct user name'))
+      if (!value) {
+        callback(new Error('账号不能为空'))
       } else {
         callback()
       }
     }
     const validatePassword = (rule, value, callback) => {
-      if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
+      if (!value) {
+        callback(new Error('密码不能为空'))
       } else {
         callback()
       }
